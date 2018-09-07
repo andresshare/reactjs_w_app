6fdfa760a715a05afc0aeb3d695edcba709068fe
andresshare <andresshare@gmail.com>
2018-09-06 20:35:53 -0500

    create  2  components from canvas

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Weather App</h1>
         <WeatherLocation></WeatherLocation>
       </div>
     );
--- src/components/WeatherLocation.js
+++ src/components/WeatherLocation.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Location 
+from './Location'
+
+import WeatherData
+from './WeatherData'
+
+
 const  WeatherLocation = () =>(
     <div>
-        Weather Location
-        
+        <Location></Location>
+        <WeatherData></WeatherData>
     </div>
 )
 export default WeatherLocation;
